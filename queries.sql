7a3ef1b2a4b823b17523cabdc4d2cf69b16a55a5
Andrash2025 <m4github@gmail.com>
2025-11-13 17:51:11 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,3 @@
+SELECT					-- ¬ыбераем
+COUNT(customer_id) as customers_count 	-- счет по полколонке customer_id с указанием псевдонима колонки как customers_count
+FROM customers 				-- из таблицы customers
