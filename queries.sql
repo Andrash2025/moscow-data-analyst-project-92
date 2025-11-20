c64b2980ddc5e4d0439bf3b2d116407888f357f8
Andrash2025 <m4github@gmail.com>
2025-11-20 12:11:22 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,120 +1,212 @@
-SELECT					-- Выбераем
-COUNT(customer_id) as customers_count 	-- счет по полколонке customer_id с указанием псевдонима колонки как customers_count
-FROM customers 				-- из таблицы customers
+-- Выбераем счет по полколонке ОК
+--customer_id с указанием псевдонима колонки
+--как customers_count из таблицы customers
+SELECT COUNT(customer_id) AS customers_count
+FROM customers
 
--- 5 задание в проекте
---1.Подготовьте в файл top_10_total_income.csv отчет с продавцами у которых наибольшая выручка
-select                           					-- Выбираем поля
-  concat(e.first_name,' ', e.last_name) as seller,  			-- Выбираем first_name и last_name "склеевая" их с помощью функции concat и присваевая псевдоним seller
-  COUNT(s.sales_id) as operation,            				-- Считаем количество продаж каждого менеджера по колонке sales_id присваевая псевдоним operation
-  Floor(SUM (s.quantity * p.price )) as income   			-- Создаем новую колонку сумарная выручка продавца за весь период умножая количество продаж на сумму кругляя в меньшую сторону income
-FROM sales as s                      					-- Левая таблица sales устанавливаем псевдоним s  
-left join employees as e                				-- Присоеденяем левым соединением таблицу employees устанавливаем псевдоним e
-on s.sales_person_id = e.employee_id          				-- Соединяем две таблицы по столбцам s.sales_person_id = e.employee_id
-left join products as p                  				-- Присоеденяем левым соединением таблицу products устанавливаем псевдоним p
-on s.product_id = p.product_id              				-- Соединяем две таблицы по столбцам s.product_id = p.product_id
-group by seller                      					-- Группируем итоговую выборку по столбцу seller
-order by income DESC                  					-- Упорядочиваем итоговую выборку по столбцу income в порядке убывания
-limit 10;                        					-- Выводим первые 10 записей итоговой таблицы т.к. по условию задачи нам нужны  топ 10 продавцов у которых наибольшая выручка
+-- 5 задание в проекте OK
+--1.Подготовьте в файл top_10_total_income.csv 
+--отчет с продавцами у которых наибольшая выручка
+-- Выбираем поля
+-- Выбираем first_name и lASt_name "склеевая" их с помощью
+-- Создаем новую колонку сумарная выручка продавца за весь 
+--период умножая количество продаж на сумму кругляя в меньшую сторону income
+-- Левая таблица sales устанавливаем псевдоним s
+-- Присоеденяем левым соединением таблицу employees устанавливаем псевдоним e
+-- Соединяем две таблицы по столбцам s.sales_persON_id = e.employee_id
+-- Присоеденяем левым соединением таблицу products устанавливаем псевдоним p
+-- Соединяем две таблицы по столбцам s.product_id = p.product_id
+-- Группируем итоговую выборку по столбцу seller
+-- Упорядочиваем итоговую выборку по столбцу income в порядке убывания
+-- Выводим первые 10 записей итоговой таблицы т.к. 
+--по условию задачи нам нужны  топ 10 продавцов у которых наибольшая выручка
+--функции cONcat и присваевая псевдоним seller
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    COUNT(s.sales_id) AS operations,
+    FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales AS s
+LEFT JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY seller
+ORDER BY income DESC
+LIMIT 10;
 
--- 2.Подготовьте в файл lowest_average_income.csv отчет с продавцами, чья выручка ниже средней выручки всех продавцов
-with raschet as(							-- Для решения задачи используем обобщённое табличное выражение, CTE.					
-select                        						-- Выбираем поля
-  concat(e.first_name,' ', e.last_name) as seller,  			-- Выбираем first_name и last_name "склеевая" их с помощью функции concat и присваевая псевдоним seller
-  COUNT(s.sales_id) as operation,            				-- Считаем количество продаж каждого менеджера по колонке sales_id присваевая псевдоним operation
-  Floor(SUM (s.quantity * p.price )) as income,				-- Создаем новую колонку сумарная выручка продавца за весь период умножая количество продаж на сумму кругляя в менбшую сторону come
-  Floor(AVG(s.quantity * p.price)) as average_income,			-- Расчитываем среднюю выручку по каждому продавцу с помощью деления суммы его выручки на сумму количества его сделок
-  SUM(SUM(s.quantity * p.price)) over() as all_income,			-- Рассчитываем сумму выручки всех продавцов путем сложения всех сумм выручек всех продавцов
-  SUM(COUNT(s.sales_id)) over() as all_operation,	 		-- Рассчитываем сумму всех продаж всех продавцов	
-  FLOOR(SUM(SUM(s.quantity * p.price)) over()/ SUM(COUNT(s.sales_id)) over())as avg_all -- Рассчитываем сдеднее значение суммы продаж по всем продавцам с помощью оконных функций путем деления общей суммы на общее количество продаж
-FROM sales as s                      					-- Левая таблица sales устанавливаем псевдоним s  
-left join employees as e                				-- Присоеденяем левым соединением таблицу employees устанавливаем псевдоним e
-on s.sales_person_id = e.employee_id          				-- Соединяем две таблицы по столбцам s.sales_person_id = e.employee_id
-left join products as p                  				-- Присоеденяем левым соединением таблицу products устанавливаем псевдоним p
-on s.product_id = p.product_id 						-- Соединяем две таблицы по столбцам s.product_id = p.product_id
-group by seller								-- Группируем по продавцам						
+-- 2.Подготовьте в файл lowest_average_income.csv отчет с продавцами, OK
+--чья выручка ниже средней выручки всех продавцов
+-- Для решения задачи используем обобщённое табличное выражение, CTE.
+-- Выбираем поля
+-- Выбираем first_name и lASt_name "склеевая" их с помощью функции 
+--cONcat и присваевая псевдоним seller
+-- Считаем количество продаж каждого менеджера по колонке sales_id 
+--присваевая псевдоним operations
+-- Создаем новую колонку сумарная выручка продавца за весь период 
+--умножая количество продаж на сумму округляя в меньшую сторону income
+-- Расчитываем среднюю выручку по каждому продавцу с помощью деления 
+--суммы его выручки на сумму количества его сделок
+-- Рассчитываем сумму выручки всех продавцов путем сложения всех сумм 
+--выручек всех продавцов
+-- Рассчитываем сумму всех продаж всех продавцов
+-- Рассчитываем сдеднее значение суммы продаж по всем продавцам с 
+--помощью оконных функций путем деления общей суммы на общее количество продаж
+-- Левая таблица sales устанавливаем псевдоним s
+-- Присоеденяем левым соединением таблицу employees устанавливаем псевдоним e
+-- Соединяем две таблицы по столбцам s.sales_persON_id = e.employee_id
+-- Присоеденяем левым соединением таблицу products устанавливаем псевдоним p
+-- Соединяем две таблицы по столбцам s.product_id = p.product_id
+-- Группируем по продавцам
+-- Основной запрос. Выбераем поля
+-- Среднюю сумарная выручки продавца
+-- Из CTE таблицы raschet
+-- Задаем условие отбора, тех продавцов средняя общая выручка, 
+--которых меньше общей средней выручки
+-- Упорядочиваем выборку по средней выручке продавца по убывания
+WITH raschet AS (
+    SELECT
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        COUNT(s.sales_id) AS operations,
+        FLOOR(SUM(s.quantity * p.price)) AS income,
+        FLOOR(AVG(s.quantity * p.price)) AS average_income,
+        SUM(SUM(s.quantity * p.price)) OVER () AS all_income,
+        SUM(COUNT(s.sales_id)) OVER () AS all_operation,
+        FLOOR(SUM(SUM(s.quantity * p.price))
+            OVER ()
+        / SUM(COUNT(s.sales_id)) OVER ()) AS avg_all
+    FROM sales AS s
+    LEFT JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    LEFT JOIN products AS p
+        ON s.product_id = p.product_id
+    GROUP BY seller
 )
-select 									-- Основной запрос. Выбераем поля
-seller,									-- Продавец
-average_income								-- Среднюю сумарная выручки продавца
-from raschet								-- Из CTE таблицы aschet
-where average_income < avg_all						-- Задаем условие отбора, тех продавцов средняя общая выручка, которых меньше общей средней выручки
-order by average_income desc;						-- Упорядочиваем выборку по средней выручке продавца по убывания
 
---3. Подготовьте в файл day_of_the_week_income.csv отчет с данными по выручке по каждому продавцу и дню недели
-WITH sales_by_day AS (							-- Для решения задачи используем обобщённое табличное выражение, CTE.
-  select								-- Выбираем поля
-    concat(e.first_name, ' ', e.last_name) AS seller,			-- Выбираем first_name и last_name "склеевая" их с помощью функции concat и присваевая псевдоним seller
-    trim(to_char(s.sale_date, 'Day')) AS day_of_week,       		-- Выбераем день из даты с помощью Day, приобразуем в текст, удаляем лишние пробелы, присваиваем псевдоним
-    extract(isodow from s.sale_date) AS day_number,			-- Извлекаем порядковый день недели
-    FLOOR(SUM(s.quantity * p.price)) AS income				-- Создаем новую колонку сумарная выручка продавца за весь период умножая количество продаж на сумму кругляя в меньшую сторону income
-  FROM sales as s							-- Левая таблица sales устанавливаем псевдоним s
-  LEFT JOIN employees as e 						-- Присоеденяем левым соединением таблицу employees устанавливаем псевдоним e
-  on s.sales_person_id = e.employee_id 					-- Соединяем две таблицы по столбцам s.sales_person_id = e.employee_id
-  LEFT JOIN products as p 						-- Присоеденяем левым соединением таблицу products устанавливаем псевдоним p
-  on s.product_id = p.product_id					-- Соединяем две таблицы по столбцам s.product_id = p.product_id
-  GROUP BY seller, day_of_week, day_number				-- Группируем 
+SELECT
+    seller,
+    average_income
+FROM raschet
+WHERE average_income < avg_all
+ORDER BY average_income DESC;
+
+--3. Подготовьте в файл day_of_the_week_income.csv отчет с данными OK
+--по выручке по каждому продавцу и дню недели
+-- Для решения задачи используем обобщённое табличное выражение, CTE.
+-- Выбираем first_name и lASt_name "склеевая" их с помощью 
+--функции cONcat и присваевая псевдоним seller
+-- Выбераем день из даты с помощью Day, приобразуем в текст, 
+--удаляем лишние пробелы, присваиваем псевдоним
+-- Выбираем поля
+-- Извлекаем порядковый день недели
+-- Создаем новую колонку сумарная выручка продавца за весь период 
+--умножая количество продаж на сумму кругляя в меньшую сторону income
+-- Левая таблица sales устанавливаем псевдоним s
+-- Присоеденяем левым соединением таблицу employees 
+--устанавливаем псевдоним e
+-- Соединяем две таблицы по столбцам s.sales_persON_id = e.employee_id
+-- Присоеденяем левым соединением таблицу products 
+--устанавливаем псевдоним p
+-- Соединяем две таблицы по столбцам s.product_id = p.product_id
+-- Группируем
+-- Основной запрос. Выбераем поля
+-- Из CTE таблицы sales_by_day
+-- Упорядочиваем итоговую таблицу
+WITH sales_by_day AS (
+    SELECT
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        TRIM(TO_CHAR(s.sale_date, 'Day')) AS day_of_week,
+        EXTRACT(ISODOW FROM s.sale_date) AS day_number,
+        FLOOR(SUM(s.quantity * p.price)) AS income
+    FROM sales AS s
+    LEFT JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    LEFT JOIN products AS p
+        ON s.product_id = p.product_id
+    GROUP BY seller, day_of_week, day_number
 )
-select									-- Основной запрос. Выбераем поля
-  seller,														
-  day_of_week,
-  income
-FROM sales_by_day							-- Из CTE таблицы sales_by_day
-ORDER by day_number, seller;						-- Упорядочиваем итоговую таблицу
 
+SELECT
+    seller,
+    day_of_week,
+    income
+FROM sales_by_day
+ORDER BY day_number, seller;
 
--- 6 Задание проекта
-
+-- 6 Задание проекта OK
 -- 1. Подготовьте в файл age_groups.csv с возрастными группами покупателей
-select									-- Выбираем поля
-case WHEN age >= 16 and age <= 25 then '16-25'				-- Используя условный оператор case задаем условия для формирования возрастных групп
-	 WHEN age >= 26 and age <= 40 then '26-40'
-	 else '40+'
-end age_category,							-- Задаем псевдоним после разделения на группы
-COUNT(customer_id) as age_count						-- Считаем количество человек в возрвстных группах и задаем псевдоним
-from customers								-- Наименование ьаблицы из которой осуществляется выборка
-group by age_category							-- Групперуем по возрвстным категориям
-order by age_category							-- Сортируем по возрастным категориям в порядке возрастания
+-- Выбираем поля
+-- Используя условный оператор cASe задаем условия 
+--для формирования возрастных групп
+-- Задаем псевдоним после разделения на группы
+-- Считаем количество человек в возрвстных группах и задаем псевдоним
+-- Наименование ьаблицы из которой осуществляется выборка
+-- Групперуем по возрвстным категориям
+-- Сортируем по возрастным категориям в порядке возрастания
+SELECT
+    CASE
+        WHEN age >= 16 AND age <= 25 THEN '16-25'
+        WHEN age >= 26 AND age <= 40 THEN '26-40'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(customer_id) AS age_count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category
 
--- 2. Подготовьте в файл customers_by_month.csv с количеством покупателей и выручкой по месяцам
-WITH sales_by_month AS (						-- Для решения задачи используем обобщённое табличное выражение, CTE.	
-select									-- Выбираем поля
-	EXTRACT(YEAR FROM s.sale_date)::TEXT || '-' || LPAD(EXTRACT(MONTH FROM s.sale_date)::TEXT, 2, '0') AS selling_month, -- Извлекаем и склеиваем гол и месяц
-	COUNT(c.customer_id) as total_customers,			--Считаем количество клиентов
-	floor(SUM(s.quantity * p.price)) as income			-- Считаем суммы потраченных клиентами средств
-from sales as s								-- Соединяем таблицы по услокиям
-left join customers as c
-on s.customer_id = c.customer_id 
-left join products as p
-on s.product_id = p.product_id
-group by selling_month							-- Группируем тпо месяцу продажи
+-- 2. Подготовьте в файл customers_by_month.csv с количеством OK
+--покупателей и выручкой по месяцам
+-- Для решения задачи используем обобщённое табличное выражение, CTE.
+-- Для решения задачи используем обобщённое табличное выражение, CTE.
+-- Выбираем поля
+-- Извлекаем и склеиваем гол и месяц
+--Считаем количество клиентов
+-- Считаем суммы потраченных клиентами средств
+-- Соединяем таблицы по услокиям
+-- Группируем тпо месяцу продажи
+-- Основной запрос. Выбераем поля
+-- Из CTE таблицы sales_by_month
+-- Группируем данные 
+WITH sales_by_month AS (
+    SELECT
+        EXTRACT(YEAR FROM s.sale_date)
+        ::TEXT || '-' || LPAD(EXTRACT(MONTH FROM s.sale_date)::TEXT, 2, '0')
+            AS selling_month,
+        COUNT(c.customer_id) AS total_customers,
+        FLOOR(SUM(s.quantity * p.price)) AS income
+    FROM sales AS s
+    LEFT JOIN customers AS c
+        ON s.customer_id = c.customer_id
+    LEFT JOIN products AS p
+        ON s.product_id = p.product_id
+    GROUP BY selling_month
 )
-select									-- Основной запрос. Выбераем поля	
-  selling_month,														
-  total_customers,
-  income
-from sales_by_month							-- Из CTE таблицы sales_by_month			
-order by selling_month;							-- Группируем данные 
 
+SELECT
+    selling_month,
+    total_customers,
+    income
+FROM sales_by_month
+ORDER BY selling_month;
 
--- 3. Подготовьте в файл special_offer.csv с покупателями первая покупка которых пришлась на время проведения специальных акций
-with discount as (							-- Для решения задачи используем обобщённое табличное выражение, CTE.	
-  select 
-    c.customer_id,
-    concat(c.first_name, ' ', c.last_name) AS customer,
-    e.first_name || ' ' || e.last_name AS seller,
+-- 3. Подготовьте в файл special_offer.csv с покупателями ОК
+--первая покупка которых пришлась на время проведения специальных акций
+-- Для решения задачи используем обобщённое 
+--табличное выражение, CTE.
+--Использовуем оконную функцию ROW_NUMBER(), 
+--чтобы выбрать первый заказ каждого клиента
+-- Соединяем таблицы по услокиям
+-- Основной запрос
+-- Выбераем поля
+-- Из CTE таблицы discount
+-- Условия первая покупка клиента по цене 0
+-- Упоряддочиваем по customer_id
+SELECT DISTINCT ON (CONCAT(c.first_name, ' ', c.last_name))
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
     s.sale_date,
-    p.price,
-    ROW_NUMBER() over (partition by c.customer_id order by s.sale_date) as rn --Использовуем оконную функцию ROW_NUMBER(), чтобы выбрать первый заказ каждого клиента
-  from sales s
-  left join customers as c on s.customer_id = c.customer_id		-- Соединяем таблицы по услокиям
-  left join employees as e on s.sales_person_id = e.employee_id
-  left join products as p on s.product_id = p.product_id
-)
-select									-- Основной запрос
-	customer,							-- Выбераем поля
-	sale_date,
- 	seller	
-from discount								-- Из CTE таблицы discount
-where rn = 1 AND price = 0						-- Условия первая покупка клиента по цене 0
-order by customer_id;							-- Упоряддочиваем по customer_id
+    e.first_name || ' ' || e.last_name AS seller
+FROM sales AS s
+INNER JOIN customers AS c ON s.customer_id = c.customer_id
+INNER JOIN products AS p ON s.product_id = p.product_id
+INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
+WHERE p.price = 0
+ORDER BY CONCAT(c.first_name, ' ', c.last_name), s.sale_date;
